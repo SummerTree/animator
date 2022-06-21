db19963755e2d40bf39353d4ea3fd18b6635134b
ray-cast <2221870259@qq.com>
2022-06-21 21:03:59 +0800

    update

--- include/octoon/runtime/base64.h
+++ include/octoon/runtime/base64.h
@@ -2,14 +2,15 @@
 #define OCTOON_BASE64_H_
 
 #include <string>
+#include <octoon/runtime/platform.h>
 
 namespace octoon
 {
-	std::string base64_encode(const unsigned char* data, std::size_t len);
-	std::string base64_encode(const std::string& s);
-	std::string base64_encode(const std::u8string& s);
+	OCTOON_EXPORT std::string base64_encode(const unsigned char* data, std::size_t len);
+	OCTOON_EXPORT std::string base64_encode(const std::string& s);
+	OCTOON_EXPORT std::string base64_encode(const std::u8string& s);
 
-	std::string base64_decode(const std::string& s);
+	OCTOON_EXPORT std::string base64_decode(const std::string& s);
 }
 
 #endif
